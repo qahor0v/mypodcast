52cfd9544b0630ef3a50429c9bc166636e6c1b97
fluttercyber <ronaldrask1@gmail.com>
2023-04-09 20:22:17 +0500

    Initial commit8

--- lib/controllers/notification_visibility/visibility_controller.dart
+++ lib/controllers/notification_visibility/visibility_controller.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 
 class VisibleController extends GetxController {
   var isVisible = false.obs;
+  var badgeIsVisible = false.obs;
 
   void changeVisibility() {
     isVisible.value = !isVisible.value;
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,16 +1,13 @@
 import 'dart:ui';
-
 import 'package:ebook_app/pages/home_page/screens/indicator_widget.dart';
-import 'package:ebook_app/controllers/notification_visibility/visibility_controller.dart';
 import 'package:ebook_app/pages/home_page/screens/name_of_parts_widget.dart';
 import 'package:ebook_app/pages/home_page/screens/newest_books_builder_widget.dart';
+import 'package:ebook_app/pages/home_page/screens/notifications_badge.dart';
 import 'package:ebook_app/pages/home_page/screens/notifications_widget.dart';
 import 'package:ebook_app/pages/home_page/screens/popular_books_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
-import 'package:badges/badges.dart' as badges;
-
 
 class HomePage extends GetView {
   @override
@@ -20,37 +17,15 @@ class HomePage extends GetView {
 
   @override
   Widget build(BuildContext context) {
-    VisibleController vc = Get.put(VisibleController());
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: const Color(0xffF5EFE1),
         actions: [
-          Obx(
-            () => IconButton(
-              icon: vc.isVisible.value
-                  ? const Icon(
-                      IconlyBroken.close_square,
-                      color: Colors.red,
-                    )
-                  : const badges.Badge(
-                      badgeContent: Text('3',style: TextStyle(color: Colors.white),),
-                      showBadge: true,
-                      child: Icon(
-                        IconlyBroken.notification,
-                        color: Color(0xff2F2F2F),
-                      ),
-                    ),
-              onPressed: () {
-                vc.changeVisibility();
-              },
-            ),
-          ),
+          NotificationsBadgeWidget(),
           IconButton(
             icon: const Icon(IconlyBroken.search, color: Color(0xff2F2F2F)),
-            onPressed: () {
-              // HiveDatabase.getAllId();
-            },
+            onPressed: () {},
           ),
         ],
         title: const Text(
