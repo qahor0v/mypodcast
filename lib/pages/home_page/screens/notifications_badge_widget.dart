4cff7f0c37ea680b30e186a511a17164fa0d17ea
fluttercyber <ronaldrask1@gmail.com>
2023-04-10 07:46:30 +0500

    Initial commit9

--- lib/pages/home_page/screens/notifications_badge_widget.dart
+++ lib/pages/home_page/screens/notifications_badge_widget.dart
@@ -1,32 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ebook_app/services/hivedb_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
 import 'package:badges/badges.dart' as badges;
-
 import '../../../controllers/notification_visibility/visibility_controller.dart';
+import '../../../services/hivedb_notifications.dart';
 
-class NotificationsBadgeWidget extends StatelessWidget {
-  NotificationsBadgeWidget({Key? key}) : super(key: key);
-  final CollectionReference _newest_podcasts =
+class NotificationsBadge2 extends StatefulWidget {
+  static const String id = "safgadf";
+
+  NotificationsBadge2({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationsBadge2> createState() => _NotificationsBadge2State();
+}
+
+class _NotificationsBadge2State extends State<NotificationsBadge2> {
+  /// variables
+  final CollectionReference newest_podcasts =
       FirebaseFirestore.instance.collection("newest_podcasts");
+  var me = HiveDBNotifications.saveOne();
+  Iterable<String> ids = HiveDBNotifications.getAllId();
+
+  VisibleController vc = Get.put(VisibleController());
 
   @override
   Widget build(BuildContext context) {
-    VisibleController vc = Get.put(VisibleController());
-
     return StreamBuilder(
-      stream: _newest_podcasts.snapshots(),
+      stream: newest_podcasts
+          .where(FieldPath.documentId, whereNotIn: ids)
+          .snapshots(),
       builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
         if (streamSnapshot.hasData) {
-
-          for (int i = 0; i < streamSnapshot.data!.docs.length; i++) {
-            final DocumentSnapshot documentSnapshot =
-                streamSnapshot.data!.docs[i];
-            HiveDBNotifications.saveId(documentSnapshot.id);
-          }
-
           return Obx(
             () => IconButton(
               icon: vc.isVisible.value
@@ -39,7 +44,9 @@ class NotificationsBadgeWidget extends StatelessWidget {
                         streamSnapshot.data!.docs.length.toString(),
                         style: const TextStyle(color: Colors.white),
                       ),
-                      showBadge: true,
+                      showBadge: streamSnapshot.data!.docs.length.isEqual(0)
+                          ? false
+                          : true,
                       child: const Icon(
                         IconlyBroken.notification,
                         color: Color(0xff2F2F2F),
